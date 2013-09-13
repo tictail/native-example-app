3411e1cba2308f2a54fe0e5a805a38f216f06afd
Joel Besada <joel.besada@gmail.com>
2013-09-13 15:33:33 +0200

    Update with feed card example and error handling

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 (function() {
-  function appStart(store) {
-    $('.app-container button').click(share);
+  function appStart() {
+    TT.api.get('v1/me')
+      .done(fetchStoreProducts, addClickHandlers)
+      .fail(genericError);
+  }
 
-    TT.api.get('v1/me').done(fetchStoreProducts);
+  function addClickHandlers(store) {
+    $('.share-button').on('click', share);
+    $('.feed-button').on('click', { store: store }, addFeedCard);
   }
 
   function fetchStoreProducts(store) {
-    TT.api.get('v1/stores/' + store.id + '/products').done(listProductImages)  ;
+    TT.api.get('v1/stores/' + store.id + '/products')
+      .done(listProductImages)
+      .fail(genericError);
   }
 
   function listProductImages(products) {
@@ -18,6 +25,8 @@
         .attr('src', product.images[0].sizes['100'])
         .appendTo(imageContainer);
     }
+
+    TT.native.reportSize();
   }
 
   function share() {
@@ -27,6 +36,24 @@
     );
   }
 
-  TT.native.init().done(appStart);
-})();
+  function addFeedCard(e) {
+    TT.api.post('v1/stores/' + e.data.store.id + '/cards', JSON.stringify({
+      title: 'My First Native Feed Card',
+      card_type: 'native',
+      content: {
+        url: 'http://localhost:4000'
+      }
+    })).done(feedCardCreated)
+      .fail(genericError);
+  }
 
+  function feedCardCreated() {
+    TT.native.showStatus('Created');
+  }
+
+  function genericError() {
+    console.error('Something went wrong');
+  }
+
+  TT.native.init().done(appStart).fail(genericError);
+})();
